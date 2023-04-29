32e6112bf147229f13053796ef119e7990f10c75
chiseanchang0727 <luciuskyear0812@gmail.com>
2023-04-29 12:45:42 +0800

    add update_wallpaer function

--- Home made applications/Plan execution counter(Desktop).py
+++ Home made applications/Plan execution counter(Desktop).py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 from datetime import date
+from PIL import Image, ImageDraw, ImageFont
+import ctypes
 
 def calculate_date_difference():
     input_date = user_date.get()
@@ -8,7 +10,9 @@ def calculate_date_difference():
     input_date = date(year, month, day)
     current_date = date.today()
     day_difference = (current_date - input_date).days
-    result_label.config(text=f"Day difference of last Daily Plan execution: {day_difference}")
+    result_label.config(text=f"Day difference from last Daily Plan execution: {day_difference}")
+    # Update the wallpaper
+    update_wallpaper(f"Day difference from last Daily Plan execution: {day_difference}")
     # Close the application
     app.destroy()  
 
@@ -19,6 +23,26 @@ def update_date_input():
         user_date.insert(0, today.isoformat())
     else:
         user_date.delete(0, tk.END)
+
+def update_wallpaper(text):
+    # Change this to the path of your current wallpaper image
+    wallpaper_path = r"C:\Users\Sean\Pictures\wallpaper\origin.jpg"
+    
+    # Create a new image with the result text
+    img = Image.open(wallpaper_path)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("arial.ttf", 50)  # Change the font and size as needed
+    draw.text((170, 50), text, font=font, fill=(0, 0, 0))
+
+    # Save the new image
+    new_wallpaper_path = r"C:\Users\Sean\Pictures\wallpaper\display.jpg"
+    img.save(new_wallpaper_path)
+
+    # Set the new image as the wallpaper (Windows only)
+    SPI_SETDESKWALLPAPER = 0x0014
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, new_wallpaper_path, 3)
+
+
 
 # Create the main window
 app = tk.Tk()
